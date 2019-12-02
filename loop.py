dcf024581887e6a0c792b2064b7115fad703a725
mfyu <myu121@gmail.com>
2019-12-02 13:17:29 -0800

    new stocks

--- loop.py
+++ loop.py
@@ -43,18 +43,24 @@ s_and_p = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET',
 
 
 stocks = []
+new_stocks = []
+
 
 with open('all_stocks_5yr.csv', newline='') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',')
 		for row in spamreader:
 			stocks.append(row)
 
-
+#new_stocks index
+k = 0
 def highest(stock):
 	high = 0.0
 	prevyear = '2013'
+	global k
+	
 	print(stock + ' highest stock price by year:')
 	for i in range(0,len(stocks)):
+		
 		if (stocks[i][6] == stock):
 
 			currentyear = stocks[i][0][0:4]
@@ -67,6 +73,41 @@ def highest(stock):
 				high = float(stocks[i][2])
 
 			prevyear = stocks[i][0][0:4]
+			
+			new_stocks.append(stocks[i])
+
+			#calculate close_20
+			close_20 = 0
+			n=0
+			for j in range(0,20):
+				if stocks[i-j][6] == stock:
+					close_20 += float(stocks[i-j][4])
+					n+=1
+			close_20 = round(close_20/n,2)
+			new_stocks[k].append(close_20)
+
+			#ratio_20
+			ratio_20 = round(float(stocks[i][4])/close_20,4)
+			new_stocks[k].append(ratio_20)
+
+			#close_50
+			close_50 = 0
+			n=0
+			for j in range(0,50):
+				if stocks[i-j][6] == stock:
+					close_50 += float(stocks[i-j][4])
+					n+=1
+			close_50 = round(close_50/n,2)
+			new_stocks[k].append(close_50)
+
+			#ratio_50
+			ratio_50 = round(float(stocks[i][4])/close_50,4)
+			new_stocks[k].append(ratio_50)
+			
+			#print(new_stocks[k])
+			k += 1
+
+
 
 	
 def highest_loop_unrolling(stock):
@@ -111,9 +152,13 @@ def loop_highest_loop_unrolling(n):
 
 
 start = timeit.default_timer()
+n=500
+loop_highest(n)
 
-#loop_highest(1)
-loop_highest_loop_unrolling(500)
+wtr = csv.writer(open ('new_stocks/new_stocks_'+str(n) + '.csv', 'w'), delimiter=',', lineterminator='\n')
+for x in new_stocks : wtr.writerow ([x])
+#print(new_stocks)
+#loop_highest_loop_unrolling(500)
 stop = timeit.default_timer()
 
 print('Time: ', stop - start) 
